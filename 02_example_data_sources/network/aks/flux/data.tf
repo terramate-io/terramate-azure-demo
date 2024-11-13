8372ba9cf7c17deae1acfd54d490b626cd729cfa
Soren Martius <soeren.martius@gmail.com>
2024-11-13 02:16:14 -0700

    feat: deploy all stacks

--- 02_example_data_sources/network/aks/flux/data.tf
+++ 02_example_data_sources/network/aks/flux/data.tf
@@ -5,6 +5,7 @@ data "terraform_remote_state" "cluster" {
   config = {
     container_name       = "tfstate"
     key                  = "terraform/stacks/by-id/5d97b4f1-4e0a-4831-8ea8-ad2d1b90f7b5/terraform.tfstate"
+    resource_group_name  = "tfstate"
     storage_account_name = "tfstatexscz2"
   }
   depends_on = [
