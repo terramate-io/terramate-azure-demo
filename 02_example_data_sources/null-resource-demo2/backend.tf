0bfba6b12f64726f163f048b2407db56efa9e922
Soren Martius <soeren.martius@gmail.com>
2025-02-11 21:12:43 +0100

    feat: just some demo

---
+++ 02_example_data_sources/null-resource-demo2/backend.tf
@@ -0,0 +1,11 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+terraform {
+  backend "azurerm" {
+    container_name       = "tfstate"
+    key                  = "terraform/stacks/by-id/196251c4-2b86-4053-a838-7f6563ccf4f9/terraform.tfstate"
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstatexscz2"
+    use_oidc             = true
+  }
+}
---
+++ 02_example_data_sources/null-resource-demo2/stack.tm.hcl
@@ -0,0 +1,5 @@
+stack {
+  name        = "Some demo"
+  description = "Some awesome description"
+  id          = "196251c4-2b86-4053-a838-7f6563ccf4f9"
+}
