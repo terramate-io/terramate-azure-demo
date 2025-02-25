95a91b04f758657096222027ebe07b946893cf62
Soren Martius <soeren.martius@gmail.com>
2025-02-25 15:45:21 +0100

    feat: test vpc

---
+++ 01_example_outputs/another-demo/backend.tf
@@ -0,0 +1,11 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+terraform {
+  backend "azurerm" {
+    container_name       = "tfstate"
+    key                  = "terraform/stacks/by-id/0bbc3793-721f-4fdc-b964-4bff61091695/terraform.tfstate"
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstatexscz2"
+    use_oidc             = true
+  }
+}
