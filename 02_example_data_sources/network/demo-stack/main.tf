6b7e6ab052a66194b2b960b2ce0e361244d2e413
Soren Martius <soeren.martius@gmail.com>
2025-02-25 15:38:21 +0100

    feat: test vpc

---
+++ 02_example_data_sources/network/demo-stack/main.tf
@@ -0,0 +1,50 @@
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "demo-network-rg"
+  location = "West Europe"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  tags = {
+    environment = "demo"
+  }
+}
+
+# Create a network security group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "demo-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "demo"
+  }
+}
