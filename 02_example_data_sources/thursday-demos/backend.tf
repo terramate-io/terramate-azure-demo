5a7cf2d561d1fd8d7342c0d84fd400b52827ea8e
Soren Martius <soeren.martius@gmail.com>
2025-02-27 15:13:46 +0100

    feat: add a new branch

---
+++ 02_example_data_sources/thursday-demos/backend.tf
@@ -0,0 +1,11 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+terraform {
+  backend "azurerm" {
+    container_name       = "tfstate"
+    key                  = "terraform/stacks/by-id/3769de92-4ad0-43a5-874b-becbe54ce466/terraform.tfstate"
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstatexscz2"
+    use_oidc             = true
+  }
+}
---
+++ 02_example_data_sources/thursday-demos/stack.tm.hcl
@@ -0,0 +1,5 @@
+stack {
+  name        = "foo"
+  description = "foo"
+  id          = "3769de92-4ad0-43a5-874b-becbe54ce466"
+}
