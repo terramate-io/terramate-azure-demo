073e32be13379caf3b1f1a8320a14bee3a25511c
Soren Martius <soeren.martius@gmail.com>
2025-03-10 09:11:30 +0100

    feat: add configuration

---
+++ 02_example_data_sources/monday-demo/backend.tf
@@ -0,0 +1,11 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+terraform {
+  backend "azurerm" {
+    container_name       = "tfstate"
+    key                  = "terraform/stacks/by-id/ccf9cf7b-2de5-4647-a099-230dccba1f1a/terraform.tfstate"
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstatexscz2"
+    use_oidc             = true
+  }
+}
---
+++ 02_example_data_sources/monday-demo/stack.tm.hcl
@@ -0,0 +1,5 @@
+stack {
+  name        = "Monday Demo"
+  description = "This stack only exists for demo purposes"
+  id          = "ccf9cf7b-2de5-4647-a099-230dccba1f1a"
+}
