b9933304153a75de9edd1162d4b1854d46aafd3a
Soren Martius <soeren.martius@gmail.com>
2025-03-25 18:16:43 +0900

    feat: add an environment

---
+++ 02_example_data_sources/just-another-demo/backend.tf
@@ -0,0 +1,11 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+terraform {
+  backend "azurerm" {
+    container_name       = "tfstate"
+    key                  = "terraform/stacks/by-id/47881c70-7fbf-4c71-ac19-aa2f49edab9a/terraform.tfstate"
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstatexscz2"
+    use_oidc             = true
+  }
+}
---
+++ 02_example_data_sources/just-another-demo/stack.tm.hcl
@@ -0,0 +1,5 @@
+stack {
+  name        = "Demo Stack"
+  description = "Demo Stack"
+  id          = "47881c70-7fbf-4c71-ac19-aa2f49edab9a"
+}
